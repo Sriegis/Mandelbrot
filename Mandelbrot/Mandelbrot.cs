9eb5c590a509ff5c487b54be54195bf5a90ac243
Sriegis <linas.bitkevicius@gmail.com>
2017-09-13 22:47:49 +0300

    Add project files.

---
+++ Mandelbrot/Mandelbrot.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandelbrot
+{
+    public class Mandelbrot
+    {
+        public Bitmap Picture { get; }
+
+        public Mandelbrot(int dimensions)
+        {
+            Picture = GeneratePicture(dimensions);
+        }
+
+        private Bitmap GeneratePicture(int dimensions)
+        {
+            var picture = new Bitmap(dimensions, dimensions);
+            var color = Color.Empty;
+
+            Console.WriteLine("Generating Mandelbrot set...");
+            for (var x = 0; x < picture.Width; x++)
+            {
+                for (var y = 0; y < picture.Height; y++)
+                {
+                    var colVal = GetMandelbrotIterations(x, y, dimensions) % 256;
+                    color = Color.FromArgb(colVal, colVal, colVal);
+                    picture.SetPixel(x, y, color);
+                }
+            }
+
+            return picture;
+        }
+
+        private int GetMandelbrotIterations(int coordX, int coordY, int dim)
+        {
+            var x = 0d;
+            var y = 0d;
+            var xtemp = 0d;
+            var iterations = 0;
+
+            while (x * x + y * y < 4 && iterations < 1000)
+            {
+                xtemp = x * x - y * y + ScaleCoordinate(coordX, dim, -2.5d, 3.5d);
+                y = 2 * x * y + ScaleCoordinate(coordY, dim, -1d, 2d);
+                x = xtemp;
+                iterations += 1;
+            }
+
+            return iterations;
+        }
+
+        private double ScaleCoordinate(int coord, int dim, double scaleStart, double scaleLength)
+        {
+            var scaleFactor = (double)coord / dim;
+            var scaleAmount = scaleLength * scaleFactor;
+            return scaleStart + scaleAmount;
+        }
+    }
+}
---
+++ Mandelbrot/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+
+namespace Mandelbrot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var dim = 4096;
+
+            //var picture = new Bitmap(dim, dim);
+            //var color = Color.Empty;
+            //var xBox = 0;
+            //var yBox = 0;
+
+            //Console.WriteLine("Processing...");
+            //for (var x = 0; x < picture.Width; x++)
+            //{
+            //    for (var y = 0; y < picture.Height; y++)
+            //    {
+            //        xBox = x / 256;
+            //        yBox = y / 256;
+            //        color = Color.FromArgb(x % 256, y % 256, 16*xBox + yBox);
+            //        picture.SetPixel(x, y, color);
+            //    }
+            //}
+
+            //Console.WriteLine("Done!");
+            //picture.Save("Mandelbrot.bmp");
+            //Console.ReadKey();
+
+            Console.WriteLine("Processing...");
+
+            var mandebrot = new Mandelbrot(4096);
+
+            Console.WriteLine("Done!");
+
+            mandebrot.Picture.Save("Mandelbrot.bmp");
+            //Console.ReadKey();
+        }
+    }
+}
